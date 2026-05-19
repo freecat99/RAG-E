bb2b58c6dd71dd26626a0eaa54639812606d9dce
adesh <adesh9744@gmail.com>
2026-05-20 04:57:48 +0530

    Replaced HTML with React sidepanel

---
+++ sidepanel/public/content.js
@@ -0,0 +1,54 @@
+console.log("Coworker Loaded");
+
+let sidebarOpen = false;
+
+const sidebar =
+document.createElement("iframe");
+
+sidebar.src =
+chrome.runtime.getURL(
+    "index.html"
+);
+
+sidebar.id = "dsa-help-sidebar";
+
+document.body.appendChild(sidebar);
+
+chrome.runtime.onMessage.addListener(
+    (message) => {
+
+        if (
+            message.action ===
+            "toggle_sidebar"
+        ) {
+
+            sidebarOpen =
+            !sidebarOpen;
+
+            sidebar.style.right =
+            sidebarOpen
+            ? "0"
+            : "-420px";
+        }
+    }
+);
+
+chrome.runtime.onMessage.addListener(
+    (message, sender, sendResponse) => {
+
+        if (
+            message.action ===
+            "get_problem"
+        ) {
+
+            const title =
+            document.querySelector(
+                '[data-cy="question-title"]'
+            )?.innerText;
+
+            sendResponse({
+                title
+            });
+        }
+    }
+);
