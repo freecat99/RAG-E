c57452693939af2d4bf6f4a5e1c4b5e76357752b
adesh <adesh9744@gmail.com>
2026-05-20 04:24:35 +0530

    Minor changes

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,31 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Coworker Extension Installed");
 });
+
+const leetcode_origin = 'https://leetcode.com/*';
+const localhost = 'https://localhost:5500/*';
+
+
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  if (!tab.url) return;
+  const url = new URL(tab.url);
+  // Enables the side panel on leetcode.com
+  if (url.origin === leetcode_origin || url.origin === localhost) {
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: './sidebar/sidebar.html',
+      enabled: true
+    });
+  } else {
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId,
+      enabled: false
+    });
+  }
+}); 
